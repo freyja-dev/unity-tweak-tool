cadc1f0cb1d86b9371ece4f82a0ff0979811450c
Barneedhar Vigneshwar <barneedhar@gmail.com>
2012-11-28 10:21:20 +0800

    Added selective sensitivity for widgets in 10 places

--- mechanig.py
+++ mechanig.py
@@ -233,8 +233,161 @@ class Handler ():
             sc_reveal_sensitivity.set_sensitive(False)
             l_launcher_reveal.set_sensitive(False)
             l_launcher_reveal_sensitivity.set_sensitive(False)
+
+
+    # selective selection in unity-launcher - part 2
+    
+    def on_sw_launcher_transparent_active_notify(self,widget,udata=None):
+        l_launcher_transparency_scale = builder.get_object('l_launcher_transparency_scale')
+        sc_launcher_transparency = builder.get_object('sc_launcher_transparency')
+
+
+        if sw_launcher_transparent.get_active() == True:
+            l_launcher_transparency_scale.set_sensitive(True)
+            sc_launcher_transparency.set_sensitive(True)
+            
+        else:
+            l_launcher_transparency_scale.set_sensitive(False)
+            sc_launcher_transparency.set_sensitive(False)
+            
+    
+    # selective selection in unity-launcher - part 3
+    
+    def on_radio_launcher_color_cus_active_notify(self,widget,udata=None):
+        color_launcher_color_cus  = builder.get_object('color_launcher_color_cus')
+      
+        if radio_launcher_color_cus.get_active() == True:
+            color_launcher_color_cus.set_sensitive(True)
+            
+        else:
+            color_launcher_color_cus.set_sensitive(False)   
+   
+    # selective selection in unity-dash - part 1
+   
+    def on_sw_dash_blur_active_notify(self,widget,udata=None):
+        radio_dash_blur_smart = builder.get_object('radio_dash_blur_smart')
+        radio_dash_blur_static = builder.get_object('radio_dash_blur_static')
+        l_dash_blur = builder.get_object('l_dash_blur')
+        
+        if sw_dash_blur.get_active() == True:
+            radio_dash_blur_smart.set_sensitive(True)
+            radio_dash_blur_static.set_sensitive(True)
+            l_dash_blur.set_sensitive(True)
+        
+        else:
+            radio_dash_blur_smart.set_sensitive(False)
+            radio_dash_blur_static.set_sensitive(False) 
+            l_dash_blur.set_sensitive(False)
+      
+      # selective selection in unity-dash - part 2
+      
+    def on_radio_dash_color_cus_active_notify(self,widget,udata=None):
+        color_dash_color_cus = builder.get_object('color_dash_color_cus')
+        
+        
+        if radio_dash_color_cus.get_active() == True:
+            color_dash_color_cus.set_sensitive(True)
+            
+ 
+        else:
+            color_dash_color_cus.set_sensitive(False)
+            
+    # selective selection in unity-panel part 1
+  
+    def on_sw_appmenu_autohide_active_notify(self,widget,udata=None):
+        spin_menu_visible = builder.get_object('spin_menu_visible')
+        l_menu_visible = builder.get_object('l_menu_visible')
+        
+        if sw_appmenu_autohide.get_active() == True:
+            spin_menu_visible.set_sensitive(True)
+            l_menu_visible.set_sensitive(True)
+        
+        else:
+            spin_menu_visible.set_sensitive(False)
+            l_menu_visible.set_sensitive(False)
             
             
+            
+  
+    # selective selection in unity-panel  part 2
+  
+    def on_sw_transparent_panel_active_notify(self,widget,udata=None):
+        sc_panel_transparency = builder.get_object('sc_panel_transparency')
+        l_transparent_panel = builder.get_object('l_transparent_panel')
+        check_panel_opaque = builder.get_object('check_panel_opaque')
+        
+        if sw_transparent_panel.get_active() == True:
+            sc_panel_transparency.set_sensitive(True)
+            l_transparent_panel.set_sensitive(True)
+            check_panel_opaque.set_sensitive(True)
+        else:
+            sc_panel_transparency.set_sensitive(False)
+            l_transparent_panel.set_sensitive(False)
+            check_panel_opaque.set_sensitive(False)
+            
+            
+     # selective sensitivity in compiz - general
+            
+    def on_sw_compiz_zoom_active_notify(self,widget,udata=None):
+        radio_zoom_type_standard = builder.get_object('radio_zoom_type_standard')
+        radio_zoom_type_lg = builder.get_object('radio_zoom_type_lg')
+        l_compiz_zoom_type = builder.get_object('l_compiz_zoom_type')
+        
+        if sw_compiz_zoom.get_active() == True:
+            radio_zoom_type_standard.set_sensitive(True)
+            radio_zoom_type_lg.set_sensitive(True)
+            l_compiz_zoom_type.set_sensitive(True)
+        
+        else:
+            radio_zoom_type_standard.set_sensitive(False)
+            radio_zoom_type_lg.set_sensitive(False)                    
+            l_compiz_zoom_type.set_sensitive(False)
+            
+    # selective sensitivity in compiz - windows spread
+    
+    def on_sw_windows_spread_active_notify(self,widget,udata=None):
+        l_compiz_spacing = builder.get_object('l_compiz_spacing')
+        l_additional = builder.get_object('l_additional')
+        spin_compiz_spacing = builder.get_object('spin_compiz_spacing')
+        check_overlay_emblem = builder.get_object('check_overlay_emblem')
+        check_click_desktop = builder.get_object('check_click_desktop')
+        
+        if sw_windows_spread.get_active() == True:
+            l_compiz_spacing.set_sensitive(True)
+            l_additional.set_sensitive(True)
+            spin_compiz_spacing.set_sensitive(True)
+            check_overlay_emblem.set_sensitive(True)
+            check_click_desktop.set_sensitive(True)
+        
+        else:
+            l_compiz_spacing.set_sensitive(False)
+            l_additional.set_sensitive(False)
+            spin_compiz_spacing.set_sensitive(False)
+            check_overlay_emblem.set_sensitive(False)
+            check_click_desktop.set_sensitive(False)        
+
+
+   # selective sensitivity in desktop settings
+                
+    def on_sw_desktop_icon_active_notify(self,widget,udata=None):
+        check_desktop_home = builder.get_object('check_desktop_home')
+        check_desktop_networkserver = builder.get_object('check_desktop_networkserver')
+        check_desktop_trash = builder.get_object('check_desktop_trash')
+        checK_desktop_devices = builder.get_object('check_desktop_devices')
+        
+        if sw_desktop_icon.get_active() == True:
+            check_desktop_home.set_sensitive(True)
+            check_desktop_networkserver.set_sensitive(True)
+            check_desktop_trash.set_sensitive(True)
+            checK_desktop_devices.set_sensitive(True)
+        
+        
+        else:
+            check_desktop_home.set_sensitive(False)
+            check_desktop_networkserver.set_sensitive(False)   
+            check_desktop_trash.set_sensitive(False)   
+            checK_desktop_devices.set_sensitive(False)   
+                 
 # Basic builder setting up
         
 builder = Gtk.Builder()
@@ -245,9 +398,15 @@ builder.connect_signals(Handler())
 # setting up switches that sensitivize things
 
 sw_launcher_hidemode = builder.get_object('sw_launcher_hidemode')
-
-
-
+sw_launcher_transparent = builder.get_object('sw_launcher_transparent')
+radio_launcher_color_cus = builder.get_object('radio_launcher_color_cus')
+sw_dash_blur = builder.get_object('sw_dash_blur')
+radio_dash_color_cus = builder.get_object('radio_dash_color_cus')
+sw_appmenu_autohide = builder.get_object('sw_appmenu_autohide')
+sw_transparent_panel = builder.get_object('sw_transparent_panel')
+sw_compiz_zoom = builder.get_object('sw_compiz_zoom')
+sw_windows_spread = builder.get_object('sw_windows_spread')
+sw_desktop_icon = builder.get_object('sw_desktop_icon')
 
 # hide tabs of the notebook
 
