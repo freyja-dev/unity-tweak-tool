53ded003e61532d79ce80d3c096f19e86fade716
Sam Hewitt <hewittsamuel@gmail.com>
2012-12-28 14:28:23 -0500

    Fixed error in unity.py.

--- mechanig/compiz.py
+++ mechanig/compiz.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Team:
-#   J Phani Mahesh <phanimahesh@gmail.com> 
-#   Barneedhar (jokerdino) <barneedhar@ubuntu.com> 
+#   J Phani Mahesh <phanimahesh@gmail.com>
+#   Barneedhar (jokerdino) <barneedhar@ubuntu.com>
 #   Amith KK <amithkumaran@gmail.com>
 #   Georgi Karavasilev <motorslav@gmail.com>
 #   Sam Tran <samvtran@gmail.com>
@@ -29,8 +29,10 @@
 # this program; if not, see <https://www.gnu.org/licenses/gpl-3.0.txt>
 
 import os, os.path
+import cairo
 
 from gi.repository import Gtk, Gio, Gdk
+from math import pi, sqrt
 
 from .ui import ui
 from . import settings
@@ -41,7 +43,7 @@ class Compizsettings ():
         '''Handler Initialisations.
         Obtain all references here.'''
         self.builder = Gtk.Builder()
-        self.glade = (os.path.join(settings.UI_DIR, 
+        self.glade = (os.path.join(settings.UI_DIR,
                                     'compiz.ui'))
         self.container = container
 # TODO : Use os module to resolve to the full path.
@@ -50,8 +52,201 @@ class Compizsettings ():
         self.page = self.ui['nb_compizsettings']
         self.page.unparent()
 
+
+# Initialise Cairo bits
+        self.window_snapping_drawable = self.ui['draw_window_snapping']
+        self._base_window_snapping_surface = cairo.ImageSurface.create_from_png(os.path.join(settings.UI_DIR, 'monitor-window-snapping.png'))
+
+        self.hotcorners_drawable = self.ui['draw_hotcorners']
+        self._base_hotcorners_surface = cairo.ImageSurface.create_from_png(os.path.join(settings.UI_DIR, 'monitor-hotcorners.png'))
+
+        self.window_snapping_cboxes = {
+            'cbox_window_snapping_top': [0, 'top-edge-action'],
+            'cbox_window_snapping_topleft': [0, 'top-left-corner-action'],
+            'cbox_window_snapping_left': [0, 'left-edge-action'],
+            'cbox_window_snapping_bottomleft': [0, 'bottom-left-corner-action'],
+            'cbox_window_snapping_bottom': [0, 'bottom-edge-action'],
+            'cbox_window_snapping_topright': [0, 'top-right-corner-action'],
+            'cbox_window_snapping_right': [0, 'right-edge-action'],
+            'cbox_window_snapping_bottomright': [0, 'bottom-right-corner-action']
+        }
+        for box in self.window_snapping_cboxes:
+            self.window_snapping_cboxes[box][0] = gsettings.grid.get_int(self.window_snapping_cboxes[box][1])
+            self.ui[box].set_active(self.window_snapping_cboxes[box][0])
+            self.ui[box].connect("changed", self.on_cbox_window_snapping_changed, box)
+
+        self.hotcorners_cboxes = {
+            'cbox_hotcorners_top': [0, 'Top'],
+            'cbox_hotcorners_topleft': [0, 'TopLeft'],
+            'cbox_hotcorners_left': [0, 'Left'],
+            'cbox_hotcorners_bottomleft': [0, 'BottomLeft'],
+            'cbox_hotcorners_bottom': [0, 'Bottom'],
+            'cbox_hotcorners_topright': [0, 'TopRight'],
+            'cbox_hotcorners_right': [0, 'Right'],
+            'cbox_hotcorners_bottomright': [0, 'BottomRight']
+        }
+        self.hotcorner_values = {
+            'show_desktop': gsettings.core.get_string('show-desktop-edge').split('|'),
+            'expo': gsettings.expo.get_string('expo-edge').split('|'),
+            'window_spread': gsettings.scale.get_string('initiate-edge').split('|')
+        }
+
+        for box in self.hotcorners_cboxes:
+            if self.hotcorners_cboxes[box][1] in self.hotcorner_values['show_desktop']:
+                self.hotcorners_cboxes[box][0] = 1
+            elif self.hotcorners_cboxes[box][1] in self.hotcorner_values['expo']:
+                self.hotcorners_cboxes[box][0] = 2
+            elif self.hotcorners_cboxes[box][1] in self.hotcorner_values['window_spread']:
+                self.hotcorners_cboxes[box][0] = 3
+            else:
+                self.hotcorners_cboxes[box][0] = 0
+
+            self.ui[box].set_active(self.hotcorners_cboxes[box][0])
+            self.ui[box].connect("changed", self.on_cbox_hotcorners_changed, box)
         self.builder.connect_signals(self)
         self.refresh()
+
+    def on_draw_hotcorners_draw (self, window, cr):
+        self.draw_monitor(window, cr, self._base_hotcorners_surface, self.hotcorners_cboxes, 'hotcorners')
+
+    def on_draw_window_snapping_draw (self, window, cr):
+        self.draw_monitor(window, cr, self._base_window_snapping_surface, self.window_snapping_cboxes, 'window_snapping')
+
+    def draw_monitor (self, window, cr, base_surface, corner_store, cbox_title):
+        x1 = 16
+        y1 = 16
+        x2 = 284
+        y2 = 200
+        x3 = 116 # Top/bottom side left-corner
+        y3 = 73  # Left/right side top-corner
+
+        corner_width = 36
+        side_height = 16
+        left_right_width = 70
+        top_bottom_width = 68
+
+        cr.set_source_surface(base_surface)
+        cr.paint()
+        cr.set_source_rgba(221/255, 72/255, 20/255);
+
+        if corner_store['cbox_' + cbox_title + '_top'][0] != 0:
+            cr.new_path()
+            cr.move_to(x3, y1)
+            cr.line_to (x3 + top_bottom_width, y1)
+            values = self.arc_values(top_bottom_width, side_height)
+            cr.arc(x3 + (top_bottom_width / 2), y1 - values['offset'], values['radius'], pi/4 , (3 * pi)/4)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_topleft'][0] != 0:
+            cr.new_path()
+            cr.move_to(x1, y1)
+            cr.line_to(x1 + corner_width, y1)
+            cr.arc(x1, y1, corner_width, 0, pi/2)
+            cr.line_to(x1, y1)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_left'][0] != 0:
+            cr.new_path()
+            cr.move_to(x1, y3 + left_right_width)
+            cr.line_to(x1, y3)
+            values = self.arc_values(left_right_width, side_height)
+            cr.arc(x1 - values['offset'], y3 + (left_right_width / 2), values['radius'], -pi/4, pi/4)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_bottomleft'][0] != 0:
+            cr.new_path()
+            cr.move_to(x1, y2 - corner_width)
+            cr.line_to(x1, y2)
+            cr.line_to(x1 + corner_width, y2)
+            cr.arc(x1, y2, corner_width, - pi / 2, 0)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_bottom'][0] != 0:
+            cr.new_path()
+            cr.move_to(x3 + top_bottom_width, y2)
+            cr.line_to(x3, y2)
+            values = self.arc_values(top_bottom_width, side_height)
+            cr.arc(x3 + (top_bottom_width / 2), y2 + values['offset'], values['radius'], (5 * pi) / 4, (7 * pi) / 4)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_topright'][0] != 0:
+            cr.new_path()
+            cr.move_to(x2, y1)
+            cr.line_to(x2, y1 + corner_width)
+            cr.arc(x2, y1, corner_width, pi / 2, pi)
+            cr.line_to(x2, y1)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_right'][0] != 0:
+            # TODO : DRAW
+            cr.new_path()
+            cr.move_to(x2, y3)
+            cr.line_to(x2, y3 + left_right_width)
+            values = self.arc_values(left_right_width, side_height)
+            cr.arc(x2 + values['offset'], y3 + (left_right_width / 2), values['radius'], (3 * pi) / 4, (5 * pi) / 4)
+            cr.fill_preserve()
+
+        if corner_store['cbox_' + cbox_title + '_bottomright'][0] != 0:
+            cr.new_path()
+            cr.move_to(x2, y2)
+            cr.line_to(x2 - corner_width, y2)
+            cr.arc(x2, y2, corner_width, pi, (3 * pi ) / 2)
+            cr.line_to(x2, y2)
+            cr.fill_preserve()
+
+    def arc_values (self, length, height):
+        # radius = (h^2 + 1/4 length^2)/2h
+        radius = ((height**2) + (.25 * (length**2))) / (2 * height)
+        return {
+            'radius': radius,
+            'offset': sqrt((radius**2) - ((length / 2)**2))
+        }
+
+    def on_cbox_window_snapping_changed (self, combobox, cbox_id):
+        self.window_snapping_cboxes[cbox_id][0] = combobox.get_active()
+        gsettings.grid.set_int(self.window_snapping_cboxes[cbox_id][1], combobox.get_active())
+        self.window_snapping_drawable.queue_draw()
+
+    def on_cbox_hotcorners_changed (self, combobox, cbox_id):
+        self.hotcorners_cboxes[cbox_id][0] = combobox.get_active()
+        clear_corners = []
+        if combobox.get_active() == 0:
+            clear_corners = ['show_desktop', 'expo', 'window_spread']
+
+        if combobox.get_active() == 1:
+            if self.hotcorners_cboxes[cbox_id][1] not in self.hotcorner_values['show_desktop']:
+                self.hotcorner_values['show_desktop'].append(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.core.set_string('show-desktop-edge', '|'.join(self.hotcorner_values['show_desktop']))
+        else:
+            clear_corners.append('show_desktop')
+
+        if combobox.get_active() == 2:
+            if self.hotcorners_cboxes[cbox_id][1] not in self.hotcorner_values['expo']:
+                self.hotcorner_values['expo'].append(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.expo.set_string('expo-edge', '|'.join(self.hotcorner_values['expo']))
+        else:
+            clear_corners.append('expo')
+
+        if combobox.get_active() == 3:
+            if self.hotcorners_cboxes[cbox_id][1] not in self.hotcorner_values['window_spread']:
+                self.hotcorner_values['window_spread'].append(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.scale.set_string('initiate-edge', '|'.join(self.hotcorner_values['window_spread']))
+        else:
+            clear_corners.append('window_spread')
+
+        # Removing potentially conflicting bindings
+        if 'show_desktop' in clear_corners and self.hotcorners_cboxes[cbox_id][1] in self.hotcorner_values['show_desktop']:
+                self.hotcorner_values['show_desktop'].remove(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.core.set_string('show-desktop-edge', '|'.join(self.hotcorner_values['show_desktop']))
+        if 'expo' in clear_corners and self.hotcorners_cboxes[cbox_id][1] in self.hotcorner_values['expo']:
+                self.hotcorner_values['expo'].remove(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.expo.set_string('expo-edge', '|'.join(self.hotcorner_values['expo']))
+        if 'window_spread' in clear_corners and self.hotcorners_cboxes[cbox_id][1] in self.hotcorner_values['window_spread']:
+                self.hotcorner_values['window_spread'].remove(self.hotcorners_cboxes[cbox_id][1])
+                gsettings.scale.set_string('initiate-edge', '|'.join(self.hotcorner_values['window_spread']))
+
+        self.hotcorners_drawable.queue_draw()
+
 
 #=====================================================================#
 #                                Helpers                              #
@@ -97,6 +292,8 @@ class Compizsettings ():
 
         del model, close_window_key, iter_close_window_key, initiate_key, iter_initiate_key, show_desktop_key, iter_show_desktop_key
 
+        # refreshing workspace settings
+
         hsize = gsettings.core.get_int('hsize')
         vsize = gsettings.core.get_int('vsize')
 
@@ -122,6 +319,8 @@ class Compizsettings ():
         model.set_value(iter_expo_key, 1, expo_key)
 
         del model, expo_key, iter_expo_key
+
+        # refreshing windows spread settings
 
         plugins = gsettings.core.get_strv('active-plugins')
         if 'scale' in plugins:
@@ -151,6 +350,28 @@ class Compizsettings ():
 
         del model, initiate_key, iter_initiate_key, initiate_all_key, iter_initiate_all_key
 
+        # refreshing window snapping settings
+
+        plugins = gsettings.core.get_strv('active-plugins')
+        if 'grid' in plugins:
+            self.ui['sw_window_snapping'].set_active(True)
+        else:
+            self.ui['sw_window_snapping'].set_active(False)
+        del plugins
+
+        color = gsettings.grid.get_string('fill-color')
+        valid, gdkcolor = Gdk.Color.parse(color[:-2])
+        if valid:
+            self.ui['color_fill_color'].set_color(gdkcolor)
+        del color, valid, gdkcolor
+
+        color = gsettings.grid.get_string('outline-color')
+        valid, gdkcolor = Gdk.Color.parse(color[:-2])
+        if valid:
+            self.ui['color_outline_color'].set_color(gdkcolor)
+        del color, valid, gdkcolor
+
+
 # TODO : Find a clever way or set each one manually.
 # Do it the dumb way now. BIIIG refactoring needed later.
 
@@ -162,7 +383,7 @@ class Compizsettings ():
 # Apologies Gnome devs, but Glade is not our favorite.      |
 #___________________________________________________________/
 
-# ===== BEGIN: Compiz settings ===== 
+# ===== BEGIN: Compiz settings =====
 #-----BEGIN: General -----
 
      # selective sensitivity in compiz - general
@@ -255,9 +476,9 @@ class Compizsettings ():
     # selective sensitivity in compiz - workspaces
 
     def on_sw_workspace_switcher_active_notify(self, widget, udata = None):
-        dependants = ['l_horizontal_desktop', 
-                    'l_vertical_desktop', 
-                    'spin_horizontal_desktop', 
+        dependants = ['l_horizontal_desktop',
+                    'l_vertical_desktop',
+                    'spin_horizontal_desktop',
                     'spin_vertical_desktop']
 
         if widget.get_active():
@@ -317,10 +538,10 @@ class Compizsettings ():
     # selective sensitivity in compiz - windows spread
 
     def on_sw_windows_spread_active_notify(self, widget, udata = None):
-        dependants = ['l_compiz_spacing', 
-                    'spin_compiz_spacing', 
-                    'check_overlay_emblem', 
-                    'check_click_desktop', 
+        dependants = ['l_compiz_spacing',
+                    'spin_compiz_spacing',
+                    'check_overlay_emblem',
+                    'check_click_desktop',
                     'scrolledwindow_compiz_window_spread']
 
         plugins = gsettings.core.get_strv('active-plugins')
@@ -348,7 +569,7 @@ class Compizsettings ():
             gsettings.scale.set_int('overlay-icon', 0)
 
     def on_check_click_desktop_toggled(self, widget):
-    
+
         if self.ui['check_click_desktop'].get_active():
             gsettings.scale.set_boolean('show-desktop', True)
         else:
@@ -387,6 +608,39 @@ class Compizsettings ():
         gsettings.scale.reset('initiate-key')
         gsettings.scale.reset('initiate-all-key')
         self.refresh()
+
+    # Compiz - Window snapping
+    def on_sw_window_snapping_active_notify(self, widget, udata=None):
+
+        plugins = gsettings.core.get_strv('active-plugins')
+
+        if widget.get_active():
+            if 'grid' not in plugins:
+                plugins.append('grid')
+                gsettings.core.set_strv('active-plugins', plugins)
+
+        else:
+            if 'grid' in plugins:
+                plugins.remove('grid')
+                gsettings.core.set_strv('active-plugins', plugins)
+
+    def on_color_outline_color_color_set(self, widget, udata=None):
+        color = self.ui['color_outline_color'].get_color()
+        colorhash = self.color_to_hash(color)
+        gsettings.grid.set_string('outline-color', colorhash)
+
+    def on_color_fill_color_color_set(self, widget, udata=None):
+        color = self.ui['color_fill_color'].get_color()
+        colorhash = self.color_to_hash(color)
+        gsettings.grid.set_string('fill-color', colorhash)
+
+    def on_b_compiz_windowsnapping_reset_clicked(self, widget):
+        gsettings.core.reset('active-plugins')
+        gsettings.core.reset('show-desktop-edge')
+        gsettings.expo.reset('expo-edge')
+        gsettings.grid.reset('fill-color')
+        gsettings.grid.reset('outline-color')
+        gsettings.grid.reset('top-edge-action')
 
 if __name__ == '__main__':
 # Fire up the Engines
