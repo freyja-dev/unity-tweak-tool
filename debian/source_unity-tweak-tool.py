d84f63b2506a679253472abf3d9f976a2fcaaa61
J Phani Mahesh <phanimahesh@gmail.com>
2013-03-27 22:46:04 +0530

    update apport hook
    Updated it for the distribution, just in case.

--- debian/source_unity-tweak-tool.py
+++ debian/source_unity-tweak-tool.py
@@ -6,5 +6,7 @@ from apport.hookutils import *
 
 
 def add_info(report):
-    if not apport.packaging.is_distro_package(report['Package'].split()[0]):
+    if apport.packaging.is_distro_package(report['Package'].split()[0]):
+        report['CrashDB'] = '{"impl":"launchpad", "distro" : "ubuntu" }
+    else:
         report['CrashDB'] = '{"impl": "launchpad", "project": "unity-tweak-tool"}'
